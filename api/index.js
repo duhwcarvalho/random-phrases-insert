6b9083110c09830afc4fb87b60b36ec32541ed2d
duhwcarvalho <duhw.carvalho@gmail.com>
2020-07-09 19:52:17 -0300

    validation form add create response mock

--- api/index.js
+++ api/index.js
@@ -1,4 +1,33 @@
-module.exports = (req, res) => {
-  const { name = 'World' } = req.query
-  res.status(200).send(`Hello ${name}!`)
+const Yup = require('yup');
+
+module.exports = async (req, res) => {
+  try {
+    const schema = Yup.object().shape({
+      phrases: Yup.string().required(),
+      author: Yup.string().required()
+    });
+
+    await schema.validate(req.body, {
+      abortEarly: false,
+    });
+
+    return res.status(200).json({
+      message: 'Frase cadastrada com sucesso!',
+      data: res.body
+    })
+
+  } catch (error) {
+    const validationErrors = {};
+
+    if (error instanceof Yup.ValidationError) {
+        error.inner.forEach(err => {
+            validationErrors[err.path] = err.message;
+        });
+    }
+
+    return res.status(400).json({
+      message: 'Falha ao cadastrar a frase :(',
+      data: Object.keys(validationErrors).length ? validationErrors : error
+    })
+  }
 }
